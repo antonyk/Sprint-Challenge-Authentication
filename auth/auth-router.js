8faac1b8dc7ad4a73748ce1e5f69746bacfb6013
Anthony K <antonyk@users.noreply.github.com>
2020-05-22 12:12:05 -0400

    implemented register endpoint to satisfy spec

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const users = require('../database/user-model')
 
 router.post('/register', (req, res) => {
+  const user = req.body;
+  // validate
+  if (user && user.username && user.password) {
+    users.insert(user).then(result => {
+      // create the jwt and return it
+
+      res.status(200).json({})
+    })
+    .catch(err => {
+      res.status(500).json({message: "Failed to create user", error: err})
+    })
+  } else {
+    res.status(400).json({ message: "Invalid signup data"})
+  }
+
   // implement registration
 });
 
