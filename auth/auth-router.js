82fe4926f2cbc35617dece948d018613a0c37e8b
Anthony K <antonyk@users.noreply.github.com>
2020-05-22 12:25:44 -0400

    added not implemented default error for login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const users = require('../database/user-model')
 
 router.post('/register', (req, res) => {
-  const user = req.body;
+  const user = req.body
   // validate
   if (user && user.username && user.password) {
     users.insert(user).then(result => {
       // create the jwt and return it
 
-      res.status(200).json({})
+      res.status(200).json({data: result, jwt: result})
     })
     .catch(err => {
       res.status(500).json({message: "Failed to create user", error: err})
@@ -17,11 +17,13 @@ router.post('/register', (req, res) => {
     res.status(400).json({ message: "Invalid signup data"})
   }
 
-  // implement registration
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const user = req.body
+  // validate
+
+  res.status(501).send('not implemented')
 });
 
 module.exports = router;
