429d9816854d2bd27386af2ae1dcb09a28fb51f0
Anthony K <antonyk@users.noreply.github.com>
2020-05-22 12:21:28 -0400

    added 3 specs (tests) for the login endpoint

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,17 +1,16 @@
 const supertest = require('supertest')
 const server = require('../api/server')
-const usersTable = require('../database/user-model')
-
-
+// const usersTable = require('../database/user-model')
 
 describe('auth-router', () => {
+  const uname = `u${Math.random()}`
+  const pass = 'let me in!'
+
   it('can run tests', () => {
     expect(true).toBeTruthy();
   })
 
   describe('POST /register', () => {
-    const uname = `u${Math.random()}`
-    const pass = 'let me in!'
     it('returns error when trying to signup with invalid data', () => {
       return supertest(server)
         .post('/api/auth/register')
@@ -30,7 +29,7 @@ describe('auth-router', () => {
         })
     })
 
-    it('returns error when trying to signup with invalid data', () => {
+    it('returns server (db) error when trying to signup with duplicate username', () => {
       return supertest(server)
         .post('/api/auth/register')
         .send({username: uname, password: pass})
@@ -39,4 +38,35 @@ describe('auth-router', () => {
         })
     })
   })
+
+  describe('POST /login', () => {
+    it('returns error when trying to login with invalid data', () => {
+      return supertest(server)
+        .post('/api/auth/login')
+        .send({password: pass})
+        .then(response => {
+          expect(response.status).toBe(400)
+        })
+    })
+
+    it('returns error when trying to login with incorrect credentials', () => {
+      return supertest(server)
+        .post('/api/auth/login')
+        .send({username: uname, password: 'I am the wrong pass'})
+        .then(response => {
+          expect(response.status).toBe(400)
+        })
+    })
+
+    it('returns returns successfully if correct credentials are passed', () => {
+      return supertest(server)
+        .post('/api/auth/login')
+        .send({username: uname, password: pass})
+        .then(response => {
+          expect(response.status).toBe(200)
+        })
+    })
+
+  })
+
 })
